10d1e261bbab0ad3ed5629f7a91aa7b42228873f
Realm <henningraffi50@gmail.com>
2022-10-15 07:56:42 +0100

    dwas

---
+++ todolist/script.js
@@ -0,0 +1,34 @@
+var i = 0;
+var txt = "What do you have planned today?";
+
+function typeWriter() {
+	if (i < txt.length) {
+		document.getElementsByClassName("js-typewrite")[0].innerHTML +=
+			txt.charAt(i);
+		i++;
+		setTimeout(typeWriter, 100);
+	}
+}
+
+setTimeout(typeWriter, 2000);
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry);
+		if (entry.isIntersecting) {
+			entry.target.classList.add("show");
+		} else {
+			entry.target.classList.remove("show");
+		}
+	});
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+     function openNav() {
+			document.getElementById("side_navigation").style.width = "350px";
+		}
+
+		function closeNav() {
+			document.getElementById("side_navigation").style.width = "0";
+		}
